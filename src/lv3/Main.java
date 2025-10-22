199775ae429b95d4df7800c5f96c19dc2e9030b8
이용준 <abc157898@gmail.com>
2025-10-22 17:14:49 +0900

    3lv_프로젝트 생성 및 Kiosk 클래스 추가

---
+++ src/lv3/Main.java
@@ -0,0 +1,55 @@
+package lv3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<MenuItem> menuItemList = new ArrayList<>();
+
+        menuItemList.add(new MenuItem("짜장면", 6900, "맛있는 짜장면"));
+        menuItemList.add(new MenuItem("간짜장", 7500, "맛있는 간짜장"));
+        menuItemList.add(new MenuItem("간짬뽕", 9500, "맛있는 간짬뽕"));
+        menuItemList.add(new MenuItem("삼선짬뽕", 12500, "맛있는 삼선짬뽕"));
+
+
+        // 접속 문구 및 종료 선택 화면
+        System.out.println("만다린에 오신 것을 환영합니다. 메뉴를 선택하시겠습니까?[나가기]");
+        String choice = sc.next();
+
+        if ("나가기".equals(choice)) {
+            System.out.println("종료하겠습니다.");
+            System.exit(0);
+        } else {
+            System.out.println("메뉴를 불러오겠습니다.");
+        }
+        // 메뉴 불러오기
+        while (true) {
+            System.out.println("[ 만다린 메뉴 ]");
+            for (int i = 0; i < menuItemList.size(); i++) {
+                MenuItem menuItem = menuItemList.get(i);
+                System.out.println((i + 1) + ". " + menuItem.getName() + "\t|W " + menuItem.getPrice() + " \t| " + menuItem.getDescription());
+            }
+            System.out.println(0 + " 종료하기");
+            System.out.println("원하시는 메뉴를 선택하여주세요.");
+            int choiceNum = sc.nextInt();
+            if (choiceNum == 1) {
+                System.out.println("장바구니에 담겼습니다.");
+            } else if (choiceNum == 2) {
+                System.out.println("장바구니에 담겼습니다.");
+            } else if (choiceNum == 3) {
+                System.out.println("장바구니에 담겼습니다.");
+            } else if (choiceNum == 4) {
+                System.out.println("장바구니에 담겼습니다.");
+            } else if (choiceNum == 0) {
+                System.out.println("종료하겠습니다.");
+                System.exit(0);
+            } else {
+                System.out.println("잘못된 입력입니다.");
+            }
+        }
+    }
+}
---
+++ src/lv3/MenuItem.java
@@ -0,0 +1,29 @@
+package lv3;
+
+public class MenuItem {
+
+    //속성
+    //세부 메뉴에 들어갈 내용
+   String name;
+   int price;
+   String description;
+
+   //생성자
+    MenuItem(String name, int price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    //기능
+    public String getName() {
+        return name;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+}
